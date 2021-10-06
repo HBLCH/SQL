0a5bb8e6a316ebba8adfad5dea48715936da7e85
HBLCH <91760048+HBLCH@users.noreply.github.com>
2021-10-06 15:15:42 -0400

    Add files via upload

--- interns_employee.sql
+++ interns_employee.sql
@@ -35,7 +35,7 @@ CREATE TABLE `employee` (
   `Mgrld` int DEFAULT NULL,
   `DeptID` int NOT NULL,
   PRIMARY KEY (`EmpID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Abirami','Bob','10-Jan-90','Female','abirami@hcl.com',1000,2,10),(2,'Bhuvi','Rob','10-Feb-91','Female','Bhuvi@cyz',500,3,20),(3,'Chandrima','Derek','10-Mar-92','Female','@abc.com',2000,4,30),(4,'Debasis','Pringle','10-Apr-93','Male','Debasis@hcl.com',3000,5,40),(5,'Enigma','Gefree','10-May-95','Female','enigma@gmail.com',4500,6,10),(6,'Funnel','Samas','10-Jun-99','Male','funnel@hcl.com',5500,7,20),(7,'Gabriel','Lal','10-Jul-97','Male','gabriel@xyz.com',6500,8,30),(8,'Hari','Fedrik','10-Aug-98','Male','hari@hcl.com',7900,9,40),(9,'Inis','Gunjan','10-Sep-99','Female','inia@hcl.com',8900,10,10),(10,'Jaisna','Singh','10-Oct-00','Female','jasina@hcl.com',1000,NULL,20);
+INSERT INTO `employee` VALUES (1,'Abirami','Bob','10-Jan-90','Female','abirami@hcl.com',1000,2,10),(2,'Bhuvi','Rob','10-Feb-91','Female','Bhuvi@cyz',500,3,20),(3,'Chandrima','Derek','10-Mar-92','Female','@abc.com',2000,4,30),(4,'Debasis','Pringle','10-Apr-93','Male','Debasis@hcl.com',3000,5,40),(5,'Enigma','Gefree','10-May-95','Female','enigma@gmail.com',4500,6,10),(6,'Funnel','Samas','10-Jun-99','Male','funnel@hcl.com',5500,7,20),(7,'Gabriel','Lal','10-Jul-97','Male','gabriel@xyz.com',6500,8,30),(8,'Hari','Fedrik','10-Aug-98','Male','hari@hcl.com',7900,9,40),(9,'Inis','Gunjan','10-Sep-99','Female','inia@hcl.com',8900,10,10),(10,'Jaisna','Singh','10-Oct-00','Female','jasina@hcl.com',1000,NULL,20),(11,'Yash','Bob','10-Jan-90','Female','abirami@hcl.com',1000,2,10),(12,'Bharat','Rob','10-Feb-91','Female','bhuvi@hcl.com',500,2,20),(13,'Krish','Derek','10-Mar-92','female','@abc.com',2000,3,30),(14,'karina','Pringle ','10-Apr-93','Male','Debasis@hcl.com',3000,3,40),(15,'King','Gefree','10-May-95','Female','Enigma@gmail.com',4500,8,10),(16,'George','Samas','10-Jun-99','Male','funnel@hcl.com',5500,8,20),(17,'Patric','Lal','10-Jul-97','Male','gabriel@xyz.com',6500,9,40),(18,'Peter','Fedrik','10-Aug-98','Male','Hari@hcl.com',7900,9,40),(19,'Mirande','Gunjan','10-Sep-99','female','inia@hcl.com',8900,10,10);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-06 15:12:43
+-- Dump completed on 2021-10-06 15:15:13
